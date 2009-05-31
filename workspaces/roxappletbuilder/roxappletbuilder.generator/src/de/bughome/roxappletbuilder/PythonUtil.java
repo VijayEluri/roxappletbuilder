702918ee0f20d891b7756b9587a10157135c7835
Markus Pielmeier <markus.pielmeier@gmx.de>
2009-05-31 16:58:44 +0000

    Added minimal gui support.

---
+++ workspaces/roxappletbuilder/roxappletbuilder.generator/src/de/bughome/roxappletbuilder/PythonUtil.java
@@ -0,0 +1,23 @@
+package de.bughome.roxappletbuilder;
+
+public final class PythonUtil {
+	
+	private PythonUtil(){
+		throw new IllegalStateException();
+	}
+	
+	public static String multiply(final String s, final Long factor){
+		final int factorInt = factor.intValue();
+		
+		if(factor == 0) return "";
+		if(factor == 1) return s;
+		
+		final StringBuilder sb = new StringBuilder(s.length() * factorInt);
+		for(int i = 0; i < factorInt; ++i){
+			sb.append(s);
+		}
+		
+		return sb.toString();
+	}
+
+}
