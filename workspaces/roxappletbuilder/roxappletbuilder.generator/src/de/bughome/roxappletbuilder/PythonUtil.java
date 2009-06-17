2edf82bd2fb5e746e1677c65a86be9929efd2461
Markus Pielmeier <markus.pielmeier@gmx.de>
2009-06-17 08:16:34 +0000

    Many changes.

--- workspaces/roxappletbuilder/roxappletbuilder.generator/src/de/bughome/roxappletbuilder/PythonUtil.java
+++ workspaces/roxappletbuilder/roxappletbuilder.generator/src/de/bughome/roxappletbuilder/PythonUtil.java
@@ -1,5 +1,8 @@
 package de.bughome.roxappletbuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public final class PythonUtil {
 	
 	private PythonUtil(){
@@ -25,5 +28,26 @@ public final class PythonUtil {
 		
 		return sb.toString();
 	}
+    
+    public static List<String> getPackages(final String p){
+    	final String[] packageNames = p.split("[.]");
+    	
+    	final List<String> subPackages =
+    		new ArrayList<String>(packageNames.length + 1);
+    	subPackages.add("");
+    	
+    	final StringBuilder sb = new StringBuilder(p.length());
+    	for(int i = 0; i < packageNames.length; ++i){
+    		if(i > 0){
+    			sb.append(".");
+    		}
+    		
+    		sb.append(packageNames[i]);
+    		
+    		subPackages.add(sb.toString());
+    	}
+    	
+    	return subPackages;
+    }
 
 }
