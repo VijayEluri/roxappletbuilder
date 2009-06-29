f06ae5d9dad37826819614c0786c691f92f6d384
Markus Pielmeier <markus.pielmeier@gmx.de>
2009-06-29 15:27:24 +0000

    Implemented basic parts of the GUI.

---
+++ workspaces/test/Bluetooth/de/bughome/python/bluetooth/callbacks.py
@@ -0,0 +1,15 @@
+# implement your custom callback methods here
+
+import sys
+
+def onBluetoothEnabledToggled(widget):
+    pass
+
+def onDiscoverDevicesClicked(widget):
+    pass
+
+def onBluetoothEnabledUpdate(widget, model, oldValue, newValue):
+    pass
+
+def onExitClicked(widget):
+    sys.exit(0)
